77887d7297c14e17bfec3587a1270f88ccd74d7c
praakash7542-jp <jyotimaurya7542@gmail.com>
2026-05-03 00:07:20 +0530

    revise up to   function

---
+++ ten_day.js
@@ -0,0 +1,152 @@
+// 02-05-2026
+
+// let  hoisting
+// carName = "Volvo";
+// const carName;
+// console.log(carName);
+
+// let numbers =  [10,20,30,40,50];
+// numbers.copyWithin(3,1,3);
+//  console.log(numbers);
+//  // [10,20,30,20,30]
+
+//const myData = ["phone", "laptop", "book", "charger", "bag"];
+//  myData.push("mouse");
+//  console.log(myData);
+//  myData.pop();
+//  console.log(myData);
+//  myData.unshift("pen");
+//  console.log(myData);
+//  myData.shift();
+//  console.log(myData);
+// console.log(myData.at(2));
+//  const myData2  =  ["Id card","cover","ear Buds"];
+// console.log(myData.concat(myData2));
+// console.log(myData);
+// myData.copyWithin(1,2,4);
+//  console.log(myData);
+// let result = myData.entries();
+// // for(let pair of result){
+// //    console.log(pair);
+// // }
+
+// for(let pair =0;pair< myData.length;pair++){
+//     console.log(pair + ":" +myData[pair]);
+// }
+
+//what is  difference  between  for in  loop and   for  of  loop  in js
+
+//  const colors = ["red","blue","black","pink"];
+
+//   for(let a in colors){    // in  provide  index
+//        console.log(a);
+//   }
+
+//    for(let  b of colors){
+//      console.log(b);
+//    }
+
+// const myData = {
+//   name: "jp",
+//   age: 22,
+//   id: 123,
+//   location: "Mohali",
+// };
+
+//  for(let key in myData){
+//     console.log(key);
+//  }
+
+//  for(let values of Object.values(myData)){
+//      console.log(values);
+//  }
+
+// const myData = {
+//   name: "jp",
+//   age: 22,
+//   id: 123,
+//   location: "Mohali",
+//   mob : 1234567890,
+// };
+
+// for(let key in myData){
+//    console.log(key + ":" + myData[key]);
+// }
+
+// let extraCharges = 2000;
+// if(extraCharges <=200){
+//    console.log("I will   pay    Extra Charges!");
+// }else{
+//      console.log("I will not pay  any  etxtra charges!");
+// }
+
+// let extraCharges =  300;
+// let  result  = (extraCharges<=200) ? "I will  pay" : "I  will  not  pay";
+//  console.log(result);
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(true || false);
+// console.log(false || false);
+
+// const name = `  "ertyyuiioooppp
+
+// " `;
+// console.log(name);
+
+// function myFun() {
+//   console.log("hello   jp!");
+//   return "Done"
+// }
+
+// // let res = myFun();
+// // console.log(res);
+//  myFun();
+//  myFun();
+//  myFun();
+//  myFun();
+
+// Parameters - some values are sent to the function
+// Arguments - some values are received by the function
+// Function Code - some work is done inside the function
+// Return Output - some value is returned from the function
+
+
+// function myAddition(a,b,c){
+//   console.log("this  is  add   function!");
+//   return a + b + c;
+
+// }
+
+// let  res = myAddition(12,3,15);
+// console.log(res);
+
+// function multiply(a, b) {
+//   let x = a * b;
+// }
+
+// let result = multiply(4, 3);
+// console.log(result);          //  undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
